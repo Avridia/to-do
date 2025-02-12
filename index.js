bdb6b6b4a455ecf38ff162a229e53479f8e6fe20
Sarah <s.bigot4982@gmail.com>
2025-02-12 09:51:25 +0100

    endpoint y 404 configurados

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import express from "express";
 const servidor = express();
 
 if(process.env.PRUEBAS){
-    servidor.use("/pruebas",express.static("/pruebas")); // si existe una variable de entorno PRUEBAS en el fichero .env, entonces el servidor leera un fichero estatico llamado pruebas al entrar en /pruebas
+    servidor.use("/pruebas",express.static("./pruebas")); // si existe una variable de entorno PRUEBAS en el fichero .env, entonces el servidor leera un fichero estatico llamado pruebas al entrar en /pruebas
 }
 
 servidor.get("/tareas", (peticion,respuesta) => {
     respuesta.send("GET /tareas");
 })
+
+// para poder probar si funcionan post, delete, y put, hay que usar fetch desde index.html en pruebas. Y la URL que se prueba en el navegador es /pruebas. El resultado aparece en la consola
 
 servidor.post("/tareas/nueva", (peticion,respuesta) => {
     respuesta.send("POST /tareas/nueva");
@@ -27,6 +29,11 @@ servidor.put("/tareas/editar/texto/:id", (peticion,respuesta) => {
 servidor.put("/tareas/editar/estado/:id", (peticion,respuesta) => {
     respuesta.send("DELETE /tareas/editar/estado/:id");
 })
+
+servidor.use((peticion,respuesta) => { // si llego aqui significa que ninguna de las opciones anteriores encaja
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
+});
 
 
 servidor.listen(process.env.PORT); // lee el puerto que esté configurado en el entorno -> en el documento .env
